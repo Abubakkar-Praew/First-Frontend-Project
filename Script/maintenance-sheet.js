d78dc7c14859ebbdc86a3d5055258310eb999cd0
Abubakkar-Praew <abubakkarpraew@gmail.com>
2024-09-17 07:10:08 +0000

    Version Updated 0.14

--- Script/maintenance-sheet.js
+++ Script/maintenance-sheet.js
@@ -1,1 +1,9 @@
-alert("maintenance Sheet is connected")
+const img = document.querySelector(".under-maintenance-popup");
+const webContent = document.querySelector(".website-content");
+const popupBtn = document.querySelector(".under-maintenance-popup-btn");
+
+
+function closeWindow() {
+  img.classList.replace("show", "hide");
+  webContent.classList.replace("hide", "show");
+  }
