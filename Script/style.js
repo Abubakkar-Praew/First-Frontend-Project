ef913d6fe9204a5eb15e3df157a16922bb7c93db
AbubakkarGSC <abubakkargsc@gmail.com>
2024-11-28 08:42:22 +0000

    Calculator Functions Start

--- Script/style.js
+++ Script/style.js
@@ -1,8 +1,14 @@
 const mainSection = document.querySelector(".introduction");
 const calculator = document.querySelector("#Calculator");
-let display = "";
+let display = document.querySelector('#display').value;
 
 function calculatorDisplay () {
     mainSection.classList.add("hidden");
     calculator.classList.remove("hidden");
+}
+
+function percetage() {
+    let percentValue = display;
+    percentValue = eval(display)
+    alert(percentValue);
 }
