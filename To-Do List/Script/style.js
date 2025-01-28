18bf3633b06c864afae00ed36b68d686692ecfda
AbubakkarGSC <abubakkargsc@gmail.com>
2025-01-28 09:56:58 +0000

    UI and Ux improved

--- To-Do List/Script/style.js
+++ To-Do List/Script/style.js
@@ -1,13 +1,17 @@
 let signinBox = document.querySelector(".signinBox");
 let forgetPassword = document.querySelector(".forgetPasswordBox");
-let forgetPasswordButton = document.querySelector(".forgetPassword")
+let forgetPasswordButton = document.querySelector(".forgetPassword");
 let signupBox = document.querySelector(".signupBox");
+let SignUsername = document.querySelector(".SignUsername");
+let SignPassword = document.querySelector(".SignPassword");
+let SigninButton = document.querySelector(".SigninButton");
 
 function forgetFunction () {
     signinBox.classList.add("hidden");
     forgetPassword.classList.remove("hidden");
     signupBox.classList.add("hidden");
 }
+
 
 function SigninFunction () {
     signinBox.classList.remove("hidden");
@@ -20,3 +24,7 @@ function SignupFunction () {
     forgetPassword.classList.add("hidden");
     signupBox.classList.remove("hidden");
 }
+
+// function signinCondition () {
+//     alert("button was clicked");
+// }
