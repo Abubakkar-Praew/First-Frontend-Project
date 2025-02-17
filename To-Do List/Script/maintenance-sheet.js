5444ff60f70830dcbb2c0ed89fe2c59d1bdeb318
Abubakkar-Praew <abubakkarpraew@gmail.com>
2025-02-17 03:01:48 +0000

    navbar update

--- To-Do List/Script/maintenance-sheet.js
+++
@@ -1,9 +0,0 @@
-const img = document.querySelector(".under-maintenance-popup");
-const webContent = document.querySelector(".website-content");
-const popupBtn = document.querySelector(".under-maintenance-popup-btn");
-
-
-function closeWindow() {
-  img.classList.replace("show", "hide");
-  webContent.classList.replace("hide", "show");
-  }
